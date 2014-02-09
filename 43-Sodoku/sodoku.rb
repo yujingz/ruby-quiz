496ef69856286100103690a726a6a177072eed05
Yujing Zheng <hzyh64160@gmail.com>
2014-02-08 18:01:08 -0600

    refine loading

--- 43-Sodoku/sodoku.rb
+++ 43-Sodoku/sodoku.rb
@@ -1,4 +1,4 @@
-data_source = "+-------+-------+-------+
+data_source = '+-------+-------+-------+
                | _ 6 _ | 1 _ 4 | _ 5 _ |
                | _ _ 8 | 3 _ 5 | 6 _ _ |
                | 2 _ _ | _ _ _ | _ _ 1 |
@@ -10,32 +10,27 @@ data_source = "+-------+-------+-------+
                | 5 _ _ | _ _ _ | _ _ 2 |
                | _ _ 7 | 2 _ 6 | 9 _ _ |
                | _ 4 _ | 5 _ 8 | _ 7 _ |
-               +-------+-------+-------+"
+               +-------+-------+-------+'
 
-class Soduku
+class Sodoku
+  attr_accessor :board
 
   def initialize(source_str)
-    @board = load_data(source_str)
+    load_data(source_str)
     solve
   end
 
   def load_data(source_str)
-    board = Array.new
+    @board = Array.new
     source_str.each_line do |line|
-      line.delete! '^0-9_'
-      if line.length > 0
-        l = line.split('')
-        board << l.map { |e| e.to_i rescue 0 }
-      end
+      next unless line =~ /\d|_/
+      @board << line.scan(/[\d_]/).map { |e| e.to_i }
     end
-    board
   end
 
   def solve
     p @board
   end
-
 end
 
-soduku = Soduku.new(data_source)
-
+Sodoku.new(data_source)
